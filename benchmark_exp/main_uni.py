8a66b77370bd09036bcb216c7ad1ffcdd39c92e5
amueller <amueller@microsoft.com>
2024-08-17 01:59:47 +0000

    add joblib parallelism

--- benchmark_exp/main_uni.py
+++ benchmark_exp/main_uni.py
@@ -7,6 +7,7 @@ from HP_list import Optimal_Uni_algo_HP_dict
 from TSB_AD.model_wrapper import Semisupervise_AD_Pool, Unsupervise_AD_Pool, run_Semisupervise_AD, run_Unsupervise_AD
 from TSB_AD.evaluation.metrics import get_metrics
 from TSB_AD.utils.slidingWindows import find_length_rank
+from joblib import Parallel, delayed
 
 # seeding
 seed = 2024
@@ -40,6 +41,60 @@ def configure_logger(filename):
 
     return logger
 
+
+def eval_one(filename, args):
+    print('Processing:{} by {}'.format(filename, args.AD_Name))
+
+    file_path = os.path.join(args.dataset_dir, filename)
+    df = pd.read_csv(file_path).dropna().to_numpy()
+    data = df[:,0].astype(float).reshape(-1, 1)
+    label = df[:,1].astype(int)
+
+    feats = data.shape[1]
+    slidingWindow = find_length_rank(data[:,0].reshape(-1, 1), rank=1)
+    train_index = filename.split('.')[0].split('_')[-2]
+    data_train = data[:int(train_index), :]
+
+    start_time = time.time()
+
+    Run_AD_Name = args.AD_Name.split('.')[0]
+
+    if Run_AD_Name in Semisupervise_AD_Pool:
+        output = run_Semisupervise_AD(Run_AD_Name, data_train, data, **Optimal_Det_HP)
+    elif Run_AD_Name in Unsupervise_AD_Pool:
+        output = run_Unsupervise_AD(Run_AD_Name, data, **Optimal_Det_HP)
+    else:
+        raise Exception(f"{Run_AD_Name} is not defined")
+
+    end_time = time.time()
+    run_time = end_time - start_time
+
+    if isinstance(output, np.ndarray):
+        logging.info(f'Success at {filename} using {args.AD_Name} | Time cost: {run_time:.3f}s at length {len(label)}')
+        np.save(target_dir+'/'+filename.split('.')[0]+'.npy', output)
+    else:
+        logger.error(f'At {filename}: '+output)
+
+    ### whether to save the evaluation result
+    if args.save:
+        try:
+            evaluation_result = get_metrics(output, label, slidingWindow=slidingWindow)
+            print('evaluation_result: ', evaluation_result)
+            list_w = list(evaluation_result.values())
+        except:       
+            list_w = [0]*9
+        list_w.insert(0, run_time)
+        list_w.insert(0, filename)
+        write_csv.append(list_w)
+
+        ## Temp Save
+        col_w = list(evaluation_result.keys())
+        col_w.insert(0, 'Time')
+        col_w.insert(0, 'file')
+        w_csv = pd.DataFrame(write_csv, columns=col_w)
+        w_csv.to_csv(f'{args.save_dir}/{args.AD_Name}.csv', index=False)
+
+
 if __name__ == '__main__':
 
     Start_T = time.time()
@@ -52,6 +107,8 @@ if __name__ == '__main__':
     parser.add_argument('--save', type=bool, help='whether to save the evaluation result',  default=True)
     parser.add_argument('--AD_Name', type=str, help='the name of the anomaly detector to use', default='IForest')
     parser.add_argument('--resume', type=bool, help='whether to resume', default=True)
+    parser.add_argument('--n_jobs', type=int, help='number of jobs', default=1)
+
     args = parser.parse_args()
 
     ## Resume
@@ -71,55 +128,4 @@ if __name__ == '__main__':
     Optimal_Det_HP = Optimal_Uni_algo_HP_dict[args.AD_Name]
     print('Optimal_Det_HP: ', Optimal_Det_HP)
 
-    for filename in file_list:
-        if filename in Resume_file_name_list: continue
-        print('Processing:{} by {}'.format(filename, args.AD_Name))
-
-        file_path = os.path.join(args.dataset_dir, filename)
-        df = pd.read_csv(file_path).dropna().to_numpy()
-        data = df[:,0].astype(float).reshape(-1, 1)
-        label = df[:,1].astype(int)
-
-        feats = data.shape[1]
-        slidingWindow = find_length_rank(data[:,0].reshape(-1, 1), rank=1)
-        train_index = filename.split('.')[0].split('_')[-2]
-        data_train = data[:int(train_index), :]
-
-        start_time = time.time()
-
-        Run_AD_Name = args.AD_Name.split('.')[0]
-
-        if Run_AD_Name in Semisupervise_AD_Pool:
-            output = run_Semisupervise_AD(Run_AD_Name, data_train, data, **Optimal_Det_HP)
-        elif Run_AD_Name in Unsupervise_AD_Pool:
-            output = run_Unsupervise_AD(Run_AD_Name, data, **Optimal_Det_HP)
-        else:
-            raise Exception(f"{Run_AD_Name} is not defined")
-
-        end_time = time.time()
-        run_time = end_time - start_time
-
-        if isinstance(output, np.ndarray):
-            logging.info(f'Success at {filename} using {args.AD_Name} | Time cost: {run_time:.3f}s at length {len(label)}')
-            np.save(target_dir+'/'+filename.split('.')[0]+'.npy', output)
-        else:
-            logger.error(f'At {filename}: '+output)
-
-        ### whether to save the evaluation result
-        if args.save:
-            try:
-                evaluation_result = get_metrics(output, label, slidingWindow=slidingWindow)
-                print('evaluation_result: ', evaluation_result)
-                list_w = list(evaluation_result.values())
-            except:       
-                list_w = [0]*9
-            list_w.insert(0, run_time)
-            list_w.insert(0, filename)
-            write_csv.append(list_w)
-
-            ## Temp Save
-            col_w = list(evaluation_result.keys())
-            col_w.insert(0, 'Time')
-            col_w.insert(0, 'file')
-            w_csv = pd.DataFrame(write_csv, columns=col_w)
-            w_csv.to_csv(f'{args.save_dir}/{args.AD_Name}.csv', index=False)
+    Parallel(n_jobs=args.n_jobs)(delayed(eval_one)(filename, args) for filename in file_list if filename not in Resume_file_name_list)
